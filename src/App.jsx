3fafcde82f62b4c5b820e90c9dec8303c8c671d7
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-05-04 16:13:30 +0200

    gestito submit form e aggiunta nuovi articoli. fix: aggiunto-il-componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
-const [articles, setArticles] = useState([
-  {
-    id: crypto.randomUUID(),
-    title: "Primo articolo",
-    content: "Descrizione del primo articolo",
-  },
-  {
-    id: crypto.randomUUID(),
-    title: "Secondo articolo",
-    content: "Descrizione del secondo articolo",
-  }
-])
 
-const [title, setTitle] = useState("")
-const [content, setContent] = useState("")
+function App() {
+
+  const [articles, setArticles] = useState([
+    {
+      id: crypto.randomUUID(),
+      title: "Primo articolo",
+      content: "Descrizione del primo articolo",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Secondo articolo",
+      content: "Descrizione del secondo articolo",
+    }
+  ])
+
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newArticle = {
+      id: crypto.randomUUID(),
+      title: title,
+      content: content
+    };
+    setArticles([...articles, newArticle]);
+    setTitle("");
+    setContent("");
+
+  
+}}
